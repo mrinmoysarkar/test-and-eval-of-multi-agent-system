75281282568acb051634416c811372d7031136a1
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2018-04-30 16:56:24 -0400

    dummy
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

---
+++ ros_ws/src/offbordctrl/src/offb_node.cpp
@@ -0,0 +1,169 @@
+
+#include <cstdlib>
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/CommandTOL.h>
+
+mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "mavros_takeoff");
+	ros::NodeHandle n;
+
+    ////////////////////////////////////////////
+    /////////////////GUIDED/////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient cl = n.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+    mavros_msgs::SetMode srv_setMode;
+    srv_setMode.request.base_mode = 0;
+    srv_setMode.request.custom_mode = "GUIDED";
+    if(cl.call(srv_setMode)){
+        ROS_INFO("setmode send ok");// %d value:", srv_setMode.response.success);
+    }else{
+        ROS_ERROR("Failed SetMode");
+        return -1;
+    }
+
+    ////////////////////////////////////////////
+    ///////////////////ARM//////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient arming_cl = n.serviceClient<mavros_msgs::CommandBool>("/mavros/cmd/arming");
+    mavros_msgs::CommandBool srv;
+    srv.request.value = true;
+    if(arming_cl.call(srv)){
+        ROS_INFO("ARM send ok");// %d", srv.response.success);
+    }else{
+        ROS_ERROR("Failed arming or disarming");
+    }
+
+    ////////////////////////////////////////////
+    /////////////////TAKEOFF////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient takeoff_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/takeoff");
+    mavros_msgs::CommandTOL srv_takeoff;
+    srv_takeoff.request.altitude = 2;
+    srv_takeoff.request.latitude = 0;
+    srv_takeoff.request.longitude = 0;
+    srv_takeoff.request.min_pitch = 0;
+    srv_takeoff.request.yaw = 0;
+    if(takeoff_cl.call(srv_takeoff)){
+        ROS_INFO("srv_takeoff send ok");// %d", srv_takeoff.response.success);
+    }else{
+        ROS_ERROR("Failed Takeoff");
+    }
+
+
+
+    ////////////////////////////////////////////
+    /////////////////DO STUFF///////////////////
+    ////////////////////////////////////////////
+    sleep(10);
+
+    ////////////////////////////////////////////
+    ///////////////////LAND/////////////////////
+    ////////////////////////////////////////////
+    ros::ServiceClient land_cl = n.serviceClient<mavros_msgs::CommandTOL>("/mavros/cmd/land");
+    mavros_msgs::CommandTOL srv_land;
+    srv_land.request.altitude = 2;
+    srv_land.request.latitude = 0;
+    srv_land.request.longitude = 0;
+    srv_land.request.min_pitch = 0;
+    srv_land.request.yaw = 0;
+    if(land_cl.call(srv_land)){
+        ROS_INFO("srv_land send ok");// %d", srv_land.response.success);
+    }else{
+        ROS_ERROR("Failed Land");
+    }
+
+
+/*
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    
+
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
+
+    // wait for FCU connection
+    while(ros::ok() && !current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 2;
+
+    //send a few setpoints before starting
+    for(int i = 100; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+
+    int lop = 0;
+    while(ros::ok() && lop<1000){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+        local_pos_pub.publish(pose);
+
+        ros::spinOnce();
+        rate.sleep();
+        lop++;
+    }
+    
+    //arm_cmd.request.value = false;
+    //if( arming_client.call(arm_cmd) &&
+      //  arm_cmd.response.success){
+        //ROS_INFO("Vehicle disarmed");
+    //}
+    return 0;*/
+}
+
+
+
+
+
+
