21151754e52c27194ed9c340632e7d30ebc300d9
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2018-08-30 13:00:11 -0700

    altitude is taken as input in hover code
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- ros_ws/src/offbordctrl/src/hover.cpp
+++ ros_ws/src/offbordctrl/src/hover.cpp
@@ -43,6 +43,10 @@ int main(int argc, char **argv)
     cout << "input cur y: ";
     cin >> curr_y;
     cout << curr_y << " curr_y" << endl;
+
+    cout << "input altitude: ";
+    cin >> h;
+    cout << h << " height in meter" << endl;
     
 
     ros::init(argc, argv, "hover_node");
