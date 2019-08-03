36cc80f5755582f9b4f890c669541f48da748cdd
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2019-08-03 01:38:53 -0400

    vertical scenario is added and trajectory is visualized in rviz
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- simulation_ws/src/offbordctrl/script/uav_control.py
+++ simulation_ws/src/offbordctrl/script/uav_control.py
@@ -11,11 +11,14 @@ import time
 import os
 import sys
 from mavros_msgs.msg import State
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Point, Vector3
+from std_msgs.msg import Header, ColorRGBA, String
 from mavros_msgs.srv import CommandBool, ParamGet, SetMode, CommandTOL
 from math import radians, sin, cos
 import time
 import threading
+from visualization_msgs.msg import Marker
+from nav_msgs.msg import Path
 
 
 
@@ -26,6 +29,13 @@ class uavControl():
         self.stopThread = False
         self.uavno = uavno
         self.currentPosition = None
+        self.path = Path()
+        # self.count = 0
+        rospy.Subscriber('/uav'+str(self.uavno)+'/mavros/state', State, self.state_cb)
+        rospy.Subscriber('/uav'+str(self.uavno)+'/mavros/local_position/pose', PoseStamped, self.local_position_cb)
+        self.setpointPub = rospy.Publisher('/uav'+str(self.uavno)+'/mavros/setpoint_position/local', PoseStamped, queue_size=100)
+        # self.marker_publisher = rospy.Publisher('/uav'+str(self.uavno)+'/visualization_marker', Marker, queue_size=5)
+        self.path_pub = rospy.Publisher('/uav'+str(self.uavno)+'/path', Path, queue_size=10)
 
     def state_cb(self,msg):
         self.current_state = msg
@@ -58,7 +68,6 @@ class uavControl():
     	rate = rospy.Rate(loop_freq)
     	rospy.wait_for_service('/uav'+str(self.uavno)+'/mavros/cmd/land')
     	land_set = rospy.ServiceProxy('/uav'+str(self.uavno)+'/mavros/cmd/land', CommandTOL)
-    	
     	
     	for i in xrange(timeout * loop_freq):
     	    if self.current_state.armed == False:
@@ -111,6 +120,22 @@ class uavControl():
 
     def local_position_cb(self,msg):
     	self.currentPosition = msg.pose
+        # to visualize in rviz
+         
+        self.path.header = msg.header
+        self.path.poses.append(msg)
+        self.path_pub.publish(self.path)
+        
+        # marker = Marker(
+        #             type=Marker.SPHERE,
+        #             id=self.count,
+        #             lifetime=rospy.Duration(1000),
+        #             pose=msg.pose,
+        #             scale=Vector3(0.05, 0.05, 0.05),
+        #             header=msg.header,
+        #             color=ColorRGBA(0.0, 2.0, 0.0, 0.8))
+        # self.count += 1
+        # self.marker_publisher.publish(marker) 
 
     def getquaternion(self, roll, pitch, yaw):
         # return tf.transformations.quaternion_from_euler(roll, pitch, yaw)
@@ -132,25 +157,123 @@ class uavControl():
         
         return q
 
-    def loop(self):
-        rospy.Subscriber('/uav'+str(self.uavno)+'/mavros/state', State, self.state_cb)
-        rospy.Subscriber('/uav'+str(self.uavno)+'/mavros/local_position/pose', PoseStamped, self.local_position_cb)
+    def getDistance(self,x,y,z):
+        dx = self.currentPosition.position.x-x
+        dy = self.currentPosition.position.y-y
+        dz = self.currentPosition.position.z-z
+        return (dx**2+dy**2+dz**2)**0.5
 
-        self.setpointPub = rospy.Publisher('/uav'+str(self.uavno)+'/mavros/setpoint_position/local', PoseStamped, queue_size=100)
-
-        rospy.sleep(5)
+    def executeVerticalScenario(self):
+        d=1
+        deld=0.5
+        x,y,z=self.currentPosition.position.x,self.currentPosition.position.y,2.0
         rate = rospy.Rate(20.0)
-        
-
+        q = self.getquaternion(0,0,0)
         setpoint = PoseStamped()
-        setpoint.pose.position.x = self.currentPosition.position.x
-        setpoint.pose.position.y = self.currentPosition.position.y
-        setpoint.pose.position.z = 1.5
+        setpoint.pose.position.x = x
+        setpoint.pose.position.y = y
+        setpoint.pose.position.z = z
+        setpoint.pose.orientation.x = q[0]
+        setpoint.pose.orientation.y = q[1]
+        setpoint.pose.orientation.z = q[2]
+        setpoint.pose.orientation.w = q[3]
+
         for i in range(100):
             self.setpointPub.publish(setpoint)
             rate.sleep()
 
+        if self.set_mode('OFFBOARD', 5) and self.set_arm(True, 5):
+            #takeoff
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+            
+            x,y,z=x+d,y,z+d
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #gain altitude
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+            
+            x,y,z=x,y+d,z
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #same altitude
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+
+            x,y,z=x-d,y,z
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #same altitude
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+
+            x,y,z=x,y+d,z-d
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #loose altitude
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+
+            x,y,z=x+d,y,z
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #same altitude
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+
+            x,y,z=x-d+deld,y-2*d+deld,z
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #same altitude
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+
+            x,y,z=x-deld,y-deld,0.1
+            setpoint.pose.position.x = x
+            setpoint.pose.position.y = y
+            setpoint.pose.position.z = z
+            #land
+            while self.getDistance(x,y,z)>0.1:
+                self.setpointPub.publish(setpoint)
+                rate.sleep()
+
+            self.land(5)
+            rospy.sleep(10)
+            self.set_arm(False, 5)
+
+    def loop(self):
+        rospy.sleep(5)
+        rate = rospy.Rate(20.0)
+        
         q = self.getquaternion(0,0,0)
+        setpoint = PoseStamped()
+        setpoint.pose.position.x = self.currentPosition.position.x
+        setpoint.pose.position.y = self.currentPosition.position.y
+        setpoint.pose.position.z = 1.5
+        # print(setpoint)
+        setpoint.pose.orientation.x = q[0]
+        setpoint.pose.orientation.y = q[1]
+        setpoint.pose.orientation.z = q[2]
+        setpoint.pose.orientation.w = q[3]
+        for i in range(100):
+            self.setpointPub.publish(setpoint)
+            rate.sleep()
+
+        
         setpoint.pose.position.x = self.currentPosition.position.x
         setpoint.pose.position.y = self.currentPosition.position.y
         setpoint.pose.position.z = 1.5
--- simulation_ws/src/offbordctrl/script/uav_control_test.py
+++ simulation_ws/src/offbordctrl/script/uav_control_test.py
@@ -8,23 +8,24 @@ import threading
 from uav_control import uavControl
 
 if __name__ == '__main__':
+    noofUav = 2
     rospy.init_node('hover_and_land_scenario_uav', anonymous=True)
     uavs = []
-    for i in range(10):
-    	uavs.append(uavControl(i))
-    	threading.Thread(target=uavs[i].loop).start()
+    for i in range(noofUav):
+        uavs.append(uavControl(i))
+        rospy.sleep(5)
+        threading.Thread(target=uavs[i].executeVerticalScenario).start()
     while not rospy.is_shutdown():
-    	try:
-    		uavNo = raw_input('input uav no 0 to 9: ')
-    		uavNo = int(uavNo)
-    		cmd = raw_input("command: \"l\" for land and then \"q\" for exit the main loop: ")
-	        if cmd=='l':
-	            uavs[uavNo].stopThread = True
-	        elif cmd=='q':
-	        	for i in range(10):
-	            	uavs[i].stopThread = True
-	            rospy.sleep(20)
-	            break
-    	except Exception as e:
-    		pass
-        
+        try:
+            uavNo = raw_input('input uav no 0 to '+str(noofUav-1)+': ')
+            uavNo = int(uavNo)
+            cmd = raw_input("command: \"l\" for land and then \"q\" for exit the main loop: ")
+            if cmd=='l':
+                uavs[uavNo].stopThread = True
+            elif cmd=='q':
+                for i in range(noofUav):
+                    uavs[i].stopThread = True
+                rospy.sleep(20)
+                break
+        except Exception as e:
+            pass
