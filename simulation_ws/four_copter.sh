874cfbab555f6d2863fe72c38ff8a088380a48da
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2021-02-10 20:27:12 -0500

    latest code
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- simulation_ws/four_copter.sh
+++ simulation_ws/four_copter.sh
@@ -5,9 +5,10 @@ export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd):$(pwd)/Tools/sitl_gazebo
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$(pwd)/Tools/sitl_gazebo/build
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(pwd)/Tools/sitl_gazebo/models
 export SITL_GAZEBO_PATH=$(pwd)/Tools/sitl_gazebo
-#roslaunch px4 four_uav_mavros_sitl_custom.launch
-#roslaunch pie multi_copter_sim.launch
-roslaunch px4 multi_uav_mavros_sitl.launch
+# roslaunch px4 four_uav_mavros_sitl_custom.launch
+# roslaunch pie multi_copter_sim.launch
+# roslaunch px4 multi_uav_mavros_sitl_sdf.launch
+roslaunch pie four_uav_mavros_sitl_custom.launch
 
 
 
